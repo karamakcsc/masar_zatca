48c6b73a74e987d6b2eebf2d903ca8c252a0481b
karamakcsc <karamakcsc@hotmail.com>
2023-09-11 11:43:51 +0300

    first upload

--- masar_zatca/custom/sales_invoice/sales_invoice.js
+++ masar_zatca/custom/sales_invoice/sales_invoice.js
@@ -1,18 +1,19 @@
 frappe.ui.form.on('Sales Invoice', {
     on_submit: function(frm) {
 		// frappe.msgprint("hi")
+        var d = frm.doc.items[cdt][cdn];
         frappe.call({
-            method: "master_zatca.api.send_to_zatca",
+            method: "masar_zatca.api.send_to_zatca",
             args: {
                 name:frm.doc.name,
                 posting_date:frm.doc.posting_date,
                 posting_time:frm.doc.posting_time,
                 currency:frm.doc.currency,
-                item_code:frm.doc.items[0].item_code,
-                item_name:frm.doc.items[0].item_name,
-                net_amount:frm.doc.items[0].net_amount,
-                qty:frm.doc.items[0].qty,
-                qty:frm.doc.items[0].idx,
+                item_code:d.item_code,
+                item_name:d.item_name,
+                net_amount:d.net_amount,
+                qty:d.qty,
+                qty:d.idx,
             },
             callback: function(response) {
                 var result = response.message;
