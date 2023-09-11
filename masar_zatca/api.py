e7bb6822b1473ee240defdc77dd6330b1f0d9262
karamakcsc <karamakcsc@hotmail.com>
2023-09-11 15:35:58 +0300

    first upload

--- masar_zatca/api.py
+++ masar_zatca/api.py
@@ -1,15 +1,6 @@
-import frappe, erpnext
-from frappe.utils import flt, cstr, nowdate, comma_and
-from frappe import throw, msgprint, _
-from frappe.custom.doctype.custom_field.custom_field import create_custom_field
-import requests , json,datetime
-from datetime import datetime
-from frappe.utils import get_request_session
-from frappe.utils import(
-	formatdate,
-	getdate,
-	DATE_FORMAT
-)
+import json
+import frappe
+import requests
 
 @frappe.whitelist()
 def send_to_zatca(name, posting_date, posting_time, currency, item_code, item_name, net_amount, qty, idx):
@@ -36,19 +27,19 @@ def send_to_zatca(name, posting_date, posting_time, currency, item_code, item_na
     json_data = json.dumps(data, ensure_ascii=False)
 
     # Print the JSON string (optional)
-    print(json_data)
+    frappe.msgprint(json_data)
 
     # Send the JSON data as a POST request
     headers = {'Content-Type': 'application/json'}
-    response = requests.post(url, data=json_data, headers=headers)
+    response = requests.post(url, data=json_data.encode('utf-8'), headers=headers)
 
     # Check the response from the server
     if response.status_code == 200:
         # Request was successful
-        print("Request was successful")
+        frappe.msgprint("Request was successful")
         # You can also print the response content if needed:
-        # print(response.text)
+        # frappe.msgprint(response.text)
     else:
         # Request failed
-        print(f"Request failed with status code {response.status_code}")
-        print(response.text)
+        frappe.msgprint(f"Request failed with status code {response.status_code}")
+        frappe.msgprint(response.text)
--- masar_zatca/custom/sales_invoice/sales_invoice.js
+++ masar_zatca/custom/sales_invoice/sales_invoice.js
@@ -1,19 +1,18 @@
 frappe.ui.form.on('Sales Invoice', {
     on_submit: function(frm) {
-		// frappe.msgprint("hi")
         var d = frm.doc.items[cdt][cdn];
         frappe.call({
             method: "masar_zatca.api.send_to_zatca",
             args: {
-                name:frm.doc.name,
-                posting_date:frm.doc.posting_date,
-                posting_time:frm.doc.posting_time,
-                currency:frm.doc.currency,
-                item_code:d.item_code,
-                item_name:d.item_name,
-                net_amount:d.net_amount,
-                qty:d.qty,
-                qty:d.idx,
+                name: frm.doc.name,
+                posting_date: frm.doc.posting_date,
+                posting_time: frm.doc.posting_time,
+                currency: frm.doc.currency,
+                item_code: d.item_code,
+                item_name: d.item_name,
+                net_amount: d.net_amount,
+                qty: d.qty,
+                idx: d.idx,
             },
             callback: function(response) {
                 var result = response.message;
